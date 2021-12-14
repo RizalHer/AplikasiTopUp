5141e1068f19943bf35bf1eff7b606aa32cf2456
ErgiAr <93042906+ErgiAr@users.noreply.github.com>
2021-12-14 15:29:01 +0700

    AliefStore
    store alief

--- app/src/main/java/com/rizal/aplikasitopup/TopUp.kt
+++ app/src/main/java/com/rizal/aplikasitopup/TopUp.kt
@@ -1,16 +1,22 @@
 package com.rizal.aplikasitopup
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.annotation.Nullable
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 class TopUp : Fragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,4 +29,6 @@ class TopUp : Fragment() {
     override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
+
+
 }
